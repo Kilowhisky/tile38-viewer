92cd876cf98757a1317b548b6a49b901d9d99d7b
Chris Rice <chris.rice@lytx.com>
2024-08-01 20:36:53 -0700

    Add progress spinner for ttl component

--- src/renderer/src/components/ItemTtl.tsx
+++ src/renderer/src/components/ItemTtl.tsx
@@ -1,19 +1,26 @@
 import { useEffect, useState } from "react";
 import { TtlResponse } from "../lib/tile38Connection.models";
 import { useTile38 } from "../lib/tile38Connection.store";
+import { CircularProgress } from "@mui/material";
 
 
 export function ItemTtl({ itemKey, id }: { itemKey: string, id: string }) {
   const tile38 = useTile38();
   const [ttl, setTtl] = useState<number>(-1);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function load(key: string, id: string) {
       const response = await tile38.connection!.raw<TtlResponse>(`TTL ${key} ${id}`);
       setTtl(response.ttl);
+      setLoading(false);
     }
     load(itemKey, id);
   }, [tile38, itemKey, id]);
+
+  if (loading) {
+    return <CircularProgress size={"1em"} sx={{ verticalAlign: 'middle' }} />
+  }
 
   if (ttl == -1 || !ttl) {
     return '∞';
