c3fd0bfccf81261e466655d0740f913d31533db2
Chris Rice <chris.rice@lytx.com>
2024-08-05 08:32:21 -0700

    Stupid linter causing compile errors

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,7 +2,7 @@ import { app, shell, BrowserWindow, ipcMain } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
-import { autoUpdater } from 'electron-updater';
+//import { autoUpdater } from 'electron-updater';
 
 function createWindow(): void {
   // Create the browser window.
